49192147f466ef52f2f9c41382efbed55980d6df
Osnir <osnirmaster@hotmail.com>
2018-12-05 10:40:52 -0200

    Update index.js

--- contatos-microservices/api-gateway/index.js
+++ contatos-microservices/api-gateway/index.js
@@ -11,6 +11,13 @@ const consultarServiceProxy = httpProxy('http://localhost:3002');
 const incluirServiceProxy = httpProxy('http://localhost:3001');
 const deletarServiceProxy = httpProxy('http://localhost:3004');
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // Rotas Proxy
 
 //alterar microservice
